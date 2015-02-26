bd821eee9061215d7eaa19886046b3913f5bbd6b
twistedretard <kenny@kento.se>
2015-02-26 15:36:39 +0100

    MOCK THE FUCK OUT OF IT!

--- src/Server/MockConnectionHandler.java
+++ src/Server/MockConnectionHandler.java
@@ -1,8 +1,66 @@
 package Server;
 
+import static org.mockito.Mockito.*;
+
+import java.net.Socket;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.Answers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.w3c.dom.NamedNodeMap;
+
 public class MockConnectionHandler {
-	public MockConnectionHandler() {
+	ServerThread server_handler = null;
+	ConnectionHandler conn_handler = null;
+	@Before
+	public void thisIsStupid() {
+		MockitoAnnotations.initMocks(this);
+		server_handler = spy(new ServerThread());
+		conn_handler = mock(ConnectionHandler.class);
+		//doNothing().when(ch).request_handling(any(NamedNodeMap.class));
+		/*doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				System.out.println("THIS IS A FUCKING VOID!");
+				return null;
+			}
+			
+		}).when(ch).run();*/
+		//doReturn(ch).when(lol).newConnectionHandler(any(Socket.class));
+		
+		//Following prints "called with arguments: foo"
+		//System.out.println(mock.someMethod("foo"));
 
 	}
+	
+	@Test
+	public void serverDeclinesConnection() {
+		when(server_handler.newConnectionHandler(any(Socket.class))).then(new Answer<ConnectionHandler>() {
 
+			@Override
+			public ConnectionHandler answer(InvocationOnMock invocation) throws Throwable {
+		        Object[] args = invocation.getArguments();
+		        Socket insocket = ((Socket)args[0]);
+		        System.out.println("Refused connection from kuk(en)!");
+				//return (ConnectionHandler)((Socket)args[0]);
+		        insocket.close();
+		        server_handler.stopRunning();
+				return null;
+			
+			
+			}});
+		server_handler.run();
+		//System.out.println(verify(lol).newConnectionHandler(any(Socket.class)));
+	}
+	
+	@Test
+	public void clientClosesConnection() {
+	}
 }
--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -11,10 +11,16 @@ public class ServerThread implements Runnable {
 
 	public ServerThread() {
 		try {
-			myService = new ServerSocket(4441);
+			myService = new ServerSocket(4444);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		Runtime.getRuntime().addShutdownHook(new Thread(){public void run(){
+		    try {
+		        myService.close();
+		        System.out.println("The server is shut down!");
+		    } catch (IOException e) {}
+		}});
 	}
 
 	@Override
@@ -22,7 +28,9 @@ public class ServerThread implements Runnable {
 		try {
 			while (!turn_off) {
 				Socket s = myService.accept();
-				new Thread(new ConnectionHandler(s)).start();
+				System.out.println("You suck stoopid½!");
+				ConnectionHandler ch = newConnectionHandler(s);
+				new Thread(ch).start();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -36,6 +44,10 @@ public class ServerThread implements Runnable {
 			myService.close();
 		} catch (IOException e) {
 		}
+	}
+	
+	public ConnectionHandler newConnectionHandler(Socket s) {
+		return new ConnectionHandler(s);
 	}
 
 }
